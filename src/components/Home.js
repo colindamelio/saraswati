8b23f615ca295dab72c9971e045a3a0adf65bc99
Colin D'Amelio <colin.damelio@gmail.com>
2018-08-29 14:30:58 -0400

    cleanup nav

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,14 @@ export default class Home extends Component {
   render() {
     return (
       <div>
-        <h1>Home</h1>
+        <h1>
+          At Saraswati Retreats, we believe your education should be
+          extraordinary.
+        </h1>
+        <h2>
+          Learn to code your own website in the heart of Bali. Apply for our
+          14-day retreat today!
+        </h2>
       </div>
     );
   }
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,13 +5,14 @@ import styled from "styled-components";
 const Link = styled(NavLink)`
   color: ${props => props.theme.colors.white};
   padding: 0 10px;
+  text-transform: uppercase;
 `;
 
 const NavContainer = styled.div`
   display: flex;
+  flex-wrap: wrap;
   background: ${props => props.theme.colors.green};
   padding: 15px 0;
-  text-transform: uppercase;
 `;
 
 const Nav = styled.nav`
@@ -20,6 +21,15 @@ const Nav = styled.nav`
   align-items: center;
   flex-grow: 1;
 
+  ${Link}:first-child {
+    font-family: ${props => props.theme.fonts.primaryFont};
+    font-weight: ${props => props.theme.weights.primaryBold};
+    font-size: 30px;
+    line-height: 13px;
+    margin-top: 11px;
+    margin-right: auto;
+  }
+
   ${Link} {
     font-family: ${props => props.theme.fonts.secondaryFont};
     font-weight: ${props => props.theme.weights.secondaryBold};
@@ -27,22 +37,15 @@ const Nav = styled.nav`
   }
 `;
 
-const LogoNav = styled.nav`
-  font-family: ${props => props.theme.fonts.primaryFont};
-  font-weight: ${props => props.theme.weights.primaryBold};
-  font-size: 30px;
-`;
-
 export default class Navigation extends Component {
   render() {
     return (
       <NavContainer>
-        <LogoNav>
-          <Link to="/">Saraswati</Link>
-        </LogoNav>
         <Nav>
+          <Link to="/">Saraswati</Link>
           <Link to="/culture">Culture</Link>
           <Link to="/curriculum">Curriculum</Link>
+          <Link to="">Apply</Link>
         </Nav>
       </NavContainer>
     );
