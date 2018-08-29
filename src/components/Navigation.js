530be474dfa821f55bf6a8399bb6a273263b4590
Colin D'Amelio <colin.damelio@gmail.com>
2018-08-29 11:09:44 -0400

    start the nav

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,14 +1,37 @@
 import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
+import styled from "styled-components";
 
+const NavContainer = styled.div`
+  display: flex;
+  background: aqua;
+  padding: 15px 0;
+`;
+
+const Nav = styled.nav`
+  &:last-of-type {
+    display: flex;
+    justify-content: flex-end;
+    flex-grow: 1;
+  }
+`;
+
+const Link = styled(NavLink)`
+  color: #000;
+  padding: 0 10px;
+`;
 export default class Navigation extends Component {
   render() {
     return (
-      <nav>
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/culture">Culture</NavLink>
-        <NavLink to="/curriculum">Curriculum</NavLink>
-      </nav>
+      <NavContainer>
+        <Nav>
+          <Link to="/">Saraswati</Link>
+        </Nav>
+        <Nav>
+          <Link to="/culture">Culture</Link>
+          <Link to="/curriculum">Curriculum</Link>
+        </Nav>
+      </NavContainer>
     );
   }
 }
