e576b4c7710166f20350d6c7f276d96f2f55e09e
Colin D'Amelio <colin.damelio@gmail.com>
2018-08-30 11:27:53 -0400

    add media query func

---
+++ src/utils/mq.js
@@ -0,0 +1,38 @@
+import { css } from "styled-components";
+
+export const devices = {
+  mobile: "mobile",
+  talbet: "tablet",
+  desktop: "desktop"
+};
+
+/**
+ * styled.div`
+ *   overflow: hidden;
+ *   ${mq.mobile`
+ *    width: 100%
+ *   `}*
+ */
+
+const mq = {
+  [devices.mobile]: (...args) =>
+    css`
+      @media screen and (max-width: 767px) {
+        ${css(...args)};
+      }
+    `,
+  [devices.tablet]: (...args) =>
+    css`
+      @media screen and (min-width: 768px) and (max-width: 1023px) {
+        ${css(...args)};
+      }
+    `,
+  [devices.desktop]: (...args) =>
+    css`
+      @media screen and (min-width: 1024px) {
+        ${css(...args)};
+      }
+    `
+};
+
+export default mq;
