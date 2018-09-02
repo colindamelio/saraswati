fb664fec0b5e82dd938d15c3124a30a968c1f0c2
colindamelio <colin.damelio@gmail.com>
2018-09-02 19:19:26 -0400

    base footer

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,10 @@
-import React, { Component, Fragment } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Home from '../routes/Home';
-import Culture from '../routes/Culture';
-import Curriculum from '../routes/Curriculum';
-import Error from '../routes/Error';
+import React, { Component, Fragment } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Home from "../routes/Home";
+import Culture from "../routes/Culture";
+import Curriculum from "../routes/Curriculum";
+import Error from "../routes/Error";
+import Footer from "../components/Footer";
 
 export default class Body extends Component {
   render() {
@@ -16,6 +17,7 @@ export default class Body extends Component {
             <Route path="/curriculum" component={Curriculum} />
             <Route component={Error} />
           </Switch>
+          <Footer />
         </Fragment>
       </BrowserRouter>
     );
