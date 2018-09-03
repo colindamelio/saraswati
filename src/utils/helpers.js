455e90d6719932ba557dce355c6af4a62e039daf
Nicole Sabater <nicole.sabater@climaxmedia.com>
2018-09-03 19:51:13 -0400

    remove smallDesktop and upToDesktop

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -7,29 +7,27 @@ export const getViewportWidth = () => {
 };
 
 export const devices = {
-  mobile: "mobile",
-  tablet: "tablet",
-  smallDesktop: "smallDesktop",
-  desktop: "desktop",
-  upToDesktop: "upToDesktop"
+  mobile: 'mobile',
+  tablet: 'tablet',
+  desktop: 'desktop',
 };
 
 const devicesById = {
   [devices.mobile]: {
     id: devices.mobile,
     from: 0,
-    to: 767
+    to: 767,
   },
   [devices.tablet]: {
     id: devices.tablet,
     from: 768,
-    to: 1023
+    to: 1023,
   },
   [devices.desktop]: {
     id: devices.desktop,
     from: 1025,
-    to: 99999
-  }
+    to: 99999,
+  },
 };
 
 export const getDeviceIdFromDimensions = ({ width }) => {
