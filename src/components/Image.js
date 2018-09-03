f22e05bb0e0bbff5981bdf9310378622dc0d960a
Nicole Sabater <nicole.sabater@climaxmedia.com>
2018-09-03 18:29:19 -0400

    fix Image component

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,21 +1,10 @@
-import React from "react";
-import PropTypes from "prop-types";
-import styled from "styled-components";
+import React from 'react';
+import styled from 'styled-components';
 
-const Img = styled.img`
+const Image = styled.img`
   display: block;
   width: 100%;
   max-width: ${props => `${props.width}px`};
 `;
 
-const Image = ({ alt, src, width }) => {
-  return src ? <Img src={src} alt={alt} width={width} /> : null;
-};
-
 export default Image;
-
-Image.propTypes = {
-  src: PropTypes.string,
-  alt: PropTypes.string,
-  width: PropTypes.number
-};
