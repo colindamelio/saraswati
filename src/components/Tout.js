ccf58a3f0d31b4405e012587d12709b78c33663d
Johnathan Fong <johnathan.fong@gmail.com>
2018-09-05 22:53:00 -0400

    Updated theme variables

--- src/components/Tout.js
+++ src/components/Tout.js
@@ -14,11 +14,12 @@ const Tout = ({ image, title, description, cta }) => {
   return (
     <Container>
       {image && image.url && <Image src={image.url} alt={image.alt}/>}
-        <BodyTextLockup
-          title={title}
-          description={description}
-          cta={cta}
-        />
+      {title || description && <BodyTextLockup
+        title={title}
+        description={description}
+      /> }
+      //We need to improve the CTA logic to map through CTA array//
+      {cta}
     </Container>
   );
 };
--- src/theme/index.js
+++ src/theme/index.js
@@ -1,33 +1,45 @@
 module.exports = {
-  colors: {
-    green: "#47AFAF",
-    black: "#5A5A5A",
-    white: "#EEEEEE",
-    orange: "#C46A28"
-  },
-  fonts: {
-    primaryFont: "Karma, serif",
-    secondaryFont: "Open Sans, sans-serif"
-  },
-  weights: {
-    light: "300",
-    italic: "400i",
-    semi: "600",
-    semiItalic: "600i",
-    bold: "700",
-    boldItalic: "700i"
-  },
-  bodyTextSizing: {
-    desktop: "1rem",
-    tablet: "1rem",
-    mobile: "1rem",
-    lineHeightDesktop: "2rem",
-    lineHeightTablet: "2rem",
-    lineHeightMobile: "2rem",
-  },
-  disclaimerTextSizing: {
-    desktop: "0.75rem",
-    tablet: "0.75rem",
-    mobile: "0.625rem",
-  },
+  //Colours//
+  black: "#5A5A5A",
+  white: "#EEEEEE",
+  primaryAccent: "#47AFAF",
+  secondaryAccent: "#C46A28",
+
+  //Fonts//
+  primaryFont: "Karma, serif",
+  secondaryFont: "Open Sans, sans-serif",
+
+  //Font Weights//
+  light: "300",
+  normal: "400",
+  semiBold: "600",
+  bold: "700",
+
+  //Font Sizing//
+  heroTitleDesktop: "66px",
+  heroTitleMobile: "30px",
+  heroDescriptionDesktop: "24px",
+  heroDescriptionMobile: "24px",
+
+  subtitleDesktop: "24px",
+  subtitleMobile: "24px",
+  bodyTextDesktop: "16px",
+  bodyTextMobile: "16px",
+
+  disclaimerDesktop: "12px",
+  disclaimerMobile: "12px",
+
+  //Line Heights//
+  titleLineHeight: "2",
+  descriptionLineHeight: "1.5",
+
+  //CTAs//
+  ctaBorder: "4px solid",
+  ctaPadding: "15px 30px",
+  ctaCasing: "uppercase",
+
+  //Content Containers//
+  paddingDesktop: "50px",
+  paddingTablet: "25px",
+  paddingMobile: "15px",
 };
