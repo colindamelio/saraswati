2d00c3b1ac2582c7ba5d5e1f404db3c9380b7c01
Nicole Sabater <nicole.sabater@climaxmedia.com>
2018-09-06 10:32:45 -0400

    dates

--- src/data/form.js
+++ src/data/form.js
@@ -5,6 +5,7 @@ module.exports = {
   'form/phone/label': 'Phone Number (Please include Country code)',
   'form/location/label': 'Where are you applying from? (City, Country)',
   'form/selectRetreat/label': 'Select an upcoming retreat',
+  'form/selectRetreat/dates': ['February 10-23, 2019'],
   'form/accomplish/label':
     'What would you like to accomplish during your Saraswati Experience?',
 };
