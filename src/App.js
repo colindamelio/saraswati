f7738794c89ba37190669a1327124a7cfbfb5d86
Nicole Sabater <nicole.sabater@climaxmedia.com>
2018-09-06 02:20:31 -0400

    pass strings

--- src/App.js
+++ src/App.js
@@ -1,45 +1,47 @@
-import React, { Component } from "react";
-import { ThemeProvider } from "styled-components";
+import React, { Component } from 'react';
+import { ThemeProvider } from 'styled-components';
 import {
   getViewportWidth,
   getDeviceIdFromDimensions,
-  devices
-} from "utils/helpers";
-import theme from "./theme";
-import Body from "./components/Body";
+  devices,
+} from 'utils/helpers';
+import theme from './theme';
+import Body from './components/Body';
+import strings from './data/resource-strings';
 
 class App extends Component {
   state = {
     isMobile: false,
     isTablet: false,
-    isDesktop: false
+    isDesktop: false,
   };
 
   componentDidMount() {
     this.setViewportDimensions();
-    window.addEventListener("resize", this.setViewportDimensions);
+    window.addEventListener('resize', this.setViewportDimensions);
+    console.log(strings);
   }
 
   componentWillUnmount() {
-    window.removeEventListener("resize", this.setViewportDimensions);
+    window.removeEventListener('resize', this.setViewportDimensions);
   }
 
   setViewportDimensions = () => {
     const deviceId = getDeviceIdFromDimensions({
-      width: getViewportWidth()
+      width: getViewportWidth(),
     });
 
     this.setState({
       isMobile: deviceId === devices.mobile,
       isTablet: deviceId === devices.tablet,
-      isDesktop: deviceId === devices.desktop
+      isDesktop: deviceId === devices.desktop,
     });
   };
 
   render() {
     return (
       <ThemeProvider theme={theme}>
-        <Body {...this.state} />
+        <Body {...this.state} strings={strings} />
       </ThemeProvider>
     );
   }
--- src/components/LandingModule.js
+++ src/components/LandingModule.js
@@ -1,12 +1,11 @@
-import React from "react";
-import styled from "styled-components";
-import HeroWithContent from "./HeroWithContent";
-import H1 from "./H1";
-import BodyText from "./BodyText";
-import mainImage from "media/bali-4-min.jpg";
+import React from 'react';
+import styled from 'styled-components';
+import HeroWithContent from './HeroWithContent';
+import H1 from './H1';
+import BodyText from './BodyText';
+import mainImage from 'media/bali-4-min.jpg';
 
 const Container = styled.div`
-  background: blue;
   height: 100vh;
   display: flex;
 `;
@@ -15,18 +14,16 @@ const Semibold = styled.span`
   font-weight: ${props => props.theme.weights.secondaryBold};
 `;
 
-const LandingModule = ({ src, width, children }) => {
+const Link = styled.a`
+  color: ${props => props.theme.colors.green};
+`;
+
+const LandingModule = ({ src, width, children, strings }) => {
   return (
     <Container>
       <HeroWithContent src={mainImage} width={50}>
-        <H1>
-          At Saraswati Retreats, we believe your education should be{" "}
-          <Semibold>extraordinary</Semibold>.
-        </H1>
-        <BodyText>
-          Learn to code your own website in the heart of Bali. Apply for our
-          14-day retreat today!
-        </BodyText>
+        <H1>{strings['landing/first/title'](Semibold)}</H1>
+        <BodyText>{strings['landing/first/description'](Link)}</BodyText>
       </HeroWithContent>
     </Container>
   );
