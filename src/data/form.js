8f09a23f170793043775ace42a9d73d95e07311f
Nicole Sabater <nicole.sabater@climaxmedia.com>
2018-09-06 02:20:19 -0400

    update strings

---
+++ src/data/form.js
@@ -0,0 +1,10 @@
+module.exports = {
+  'form/firstName/label': 'First Name',
+  'form/lastName/label': 'Last Name',
+  'form/emailAddress/label': 'Email Address',
+  'form/phone/label': 'Phone Number (Please include Country code)',
+  'form/location/label': 'Where are you applying from? (City, Country)',
+  'form/selectRetreat/label': 'Select an upcoming retreat',
+  'form/accomplish/label':
+    'What would you like to accomplish during your Saraswati Experience?',
+};
--- src/data/resource-strings.js
+++ src/data/resource-strings.js
@@ -1,5 +1,11 @@
 import landingStrings from './landing';
 import curriculumStrings from './curriculum';
 import cultureStrings from './culture';
+import formStrings from './form';
 
-export default Object.assign(landingStrings, curriculumStrings, cultureStrings);
+export default Object.assign(
+  landingStrings,
+  curriculumStrings,
+  cultureStrings,
+  formStrings
+);
