d866633b0bc7278dc33afb01a2f5d554b7676c1d
Nicole Sabater <nicole.sabater@climaxmedia.com>
2018-09-06 01:45:14 -0400

    create resource files

---
+++ src/data/resource-strings.js
@@ -0,0 +1,5 @@
+import landingStrings from './landing';
+import curriculumStrings from './curriculum';
+import cultureStrings from './culture';
+
+export default Object.assign(landingStrings, curriculumStrings, cultureStrings);
