75df1b97f692ff7d07b29da04c41ee5490416912
Johnathan Fong <johnathan.fong@gmail.com>
2018-09-07 15:06:11 -0400

    linting

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,7 +2,7 @@ import React, { Fragment } from 'react';
 import SplitHero from './SplitHero';
 import H1 from './H1';
 import ApplicationForm from './ApplicationForm';
-import reflection from 'import reflection from "media/temple-sunset.jpg";';
+import reflection from "media/temple-sunset.jpg";
 
 const Application = () => {
   return (
