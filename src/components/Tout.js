c4889df4b79701f20757224261e169320cfafc38
Nicole Sabater <nicole.sabater@climaxmedia.com>
2018-09-07 19:13:55 -0400

    ctas.length > 0

--- src/components/Tout.js
+++ src/components/Tout.js
@@ -1,9 +1,9 @@
-import React from "react";
-import PropTypes from "prop-types";
-import styled from "styled-components";
-import Image from "./Image";
-import Button from "./Button";
-import BodyTextLockup from "./BodyTextLockup";
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import Image from './Image';
+import Button from './Button';
+import BodyTextLockup from './BodyTextLockup';
 
 const Container = styled.div`
   display: flex;
@@ -14,31 +14,27 @@ const CtaContainer = styled.div`
   display: flex;
 `;
 
-
-const Tout = ({ image, title, description, secondary }) => {
-
-  const ctas = [];
-
+const Tout = ({ image, title, description, secondary, ctas }) => {
   return (
     <Container>
-      {image && image.url && <Image src={image.url} alt={image.alt}/>}
+      {image && image.url && <Image src={image.url} alt={image.alt} />}
       <BodyTextLockup
         secondary={secondary}
         title={title}
         description={description}
       />
-      {ctas &&
+      {ctas.length > 0 && (
         <CtaContainer>
-        {ctas.map((cta, n) => (
-          <Button
-            key={`${n}-cta`}
-            text={cta.title}
-            href={cta.href}
-            style={cta.style}
-          />
-        ))}
+          {ctas.map((cta, n) => (
+            <Button
+              key={`${n}-cta`}
+              text={cta.title}
+              href={cta.href}
+              style={cta.style}
+            />
+          ))}
         </CtaContainer>
-      }
+      )}
     </Container>
   );
 };
@@ -48,5 +44,5 @@ export default Tout;
 Tout.propTypes = {
   title: PropTypes.string,
   description: PropTypes.string,
-  ctas: PropTypes.array
+  ctas: PropTypes.array,
 };
