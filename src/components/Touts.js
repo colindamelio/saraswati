78611e0c7bc194480e110bfc7a0baebca07d42c9
Johnathan Fong <johnathan.fong@gmail.com>
2018-09-08 02:01:45 -0400

    fix path and add Proptypes

--- src/components/Touts.js
+++ src/components/Touts.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import mq from 'utils/mq';
-import Tout from '.././Tout';
+import Tout from './Tout';
 
 const Container = styled.div`
   display: flex;
