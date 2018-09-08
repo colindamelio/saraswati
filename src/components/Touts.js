1d2e2770a7a312da228ea6703030ea24ef02cc8f
Johnathan Fong <johnathan.fong@gmail.com>
2018-09-08 02:19:26 -0400

    linting

--- src/components/Touts.js
+++ src/components/Touts.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import PropTypes from 'prop-types';
 import mq from 'utils/mq';
 import Tout from './Tout';
 
