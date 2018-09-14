530688f14d8eccc8caa5fd9cf8e558aba1410170
Nicole Sabater <nicole.sabater@climaxmedia.com>
2018-09-14 02:32:50 -0400

    material icon

---
+++ src/components/MaterialIcon.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+import mq from 'utils/mq';
+
+const MaterialIcon = styled.i`
+  font-family: 'Material Icons';
+  font-weight: normal;
+  font-style: normal;
+  font-size: 24px;
+  display: inline-block;
+  line-height: 1;
+  text-transform: none;
+  letter-spacing: normal;
+  word-wrap: normal;
+  white-space: nowrap;
+  direction: ltr;
+  color: #eeeeee;
+  -webkit-font-smoothing: antialiased;
+  text-rendering: optimizeLegibility;
+  -moz-osx-font-smoothing: grayscale;
+  font-feature-settings: 'liga';
+`;
+
+export default MaterialIcon;
