94cbde92e97b64ee730e69cd2b7de3da4410e9a6
Johnathan Fong <johnathan.fong@gmail.com>
2018-09-14 02:01:22 -0400

    Add APPLY page

---
+++ src/routes/Apply.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react';
+import PageContent from 'modules/PageContent';
+import Application from 'modules/home/Application';
+
+const Apply = props => {
+  return (
+    <Fragment>
+      <PageContent>
+        <Application />
+      </PageContent>
+    </Fragment>
+  );
+};
+
+export default Apply;
