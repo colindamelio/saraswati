7dee2166af8054d6abd2591b3746e654d8d155ee
colindamelio <colin.damelio@gmail.com>
2018-09-13 20:31:15 -0400

    some temp error page cleanup

--- src/routes/Error.js
+++ src/routes/Error.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React from 'react';
+import styled from 'styled-components';
+import PageContent from '../modules/PageContent';
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: calc(100vh - 238px);
+`;
+
+const ErrorHeading = styled.h1`
+  text-align: center;
+`;
 
 const Error = () => {
-  return <h1>Page Does Not Exist</h1>;
+  return (
+    <PageContent>
+      <Container>
+        <ErrorHeading>
+          Sorry, the page you are looking for does not exist
+        </ErrorHeading>
+      </Container>
+    </PageContent>
+  );
 };
 
 export default Error;
